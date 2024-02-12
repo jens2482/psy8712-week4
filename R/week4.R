b8ab6c5964a89ef65b9aa614741128016f9ef304
Amanda Jensen <jens2482@umn.edu>
2024-02-11 23:32:10 -0600

    tide wide data - fix names; separate columns, change data types

--- R/week4.R
+++ R/week4.R
@@ -3,5 +3,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 
 # Data Import
-import_tbl <- read_delim ('../data/week4.dat', delim = "-", col_names = c("instance", "number", "letter", 
-                                                                          "date", "qs"))
+import_tbl <- read_delim ('../data/week4.dat', delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl)
+wide_tbl <- separate(import_tbl, qs, into = c("q1", "q2", "q3", "q4"), sep = "-")
+wide_tbl [, 5:8] <- sapply(wide_tbl[, 5:8],as.integer)
+wide_tbl$datadate <- as.POSIXct (wide_tbl$datadate, format = "%b %d, %Y HH:MM:SS") 
